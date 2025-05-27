a5e43321b1818de160f18e59eedb8cbb948c9bca
Francisco2301 <jlopezm@uamv.edu.ni>
2025-05-26 18:39:47 -0600

    actualizacion de archivos

--- kotlin+java/services/CarreraApiService.kt
+++ kotlin+java/services/CarreraApiService.kt
@@ -9,8 +9,3 @@ interface CarreraApiService {
     @POST("api/carrera")  // Asegúrate que coincida con tu endpoint real
     suspend fun crearCarrera(@Body request: CarreraRequest): Response<Map<String, Any>>
 }
-// Asegúrate de que el endpoint "/api/carrera" coincida con el que has definido en tu controlador de Spring Boot.
-// Este servicio se encargará de hacer la llamada a la API para crear una nueva carrera.
-// La respuesta se espera que sea un mapa con claves de tipo String y valores de tipo Any, lo cual es común en respuestas JSON.
-// Puedes ajustar el tipo de respuesta según lo que tu API devuelva.
-
